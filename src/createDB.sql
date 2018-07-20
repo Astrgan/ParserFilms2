76d0e4a74d9cbe6d3bc3f349d3136b6578466e50
astrgan <astrgan@gmail.com>
2018-07-20 16:28:35 +1000

    mod sql

--- src/createDB.sql
+++ src/createDB.sql
@@ -103,23 +103,17 @@ create table users(
   primary key(email)
 )ENGINE=InnoDB CHARACTER SET=UTF8;
 
-create table names(
-  name_user varchar(100) not null,
-  email varchar(100) not null,
-  primary key(name_user),
-  foreign key (email) references users(email)
-)ENGINE=InnoDB CHARACTER SET=UTF8;
-/*
 create table comments(
   id_comment int not null auto_increment,
   comment varchar(5000) not null,
-  name_user varchar(100) not null,
+  email varchar(100) not null,
   date DATETIME,
+  id_film int not null,
   primary key(id_comment),
-  foreign key (name_user) references users(name_user)
-
+  foreign key (email) references users(email),
+  foreign key (id_film) references films(id_film)
 )ENGINE=InnoDB CHARACTER SET=UTF8;
-*/
+
 create table tokens(
   token varchar(100) not null,
   email varchar(100) not null,
